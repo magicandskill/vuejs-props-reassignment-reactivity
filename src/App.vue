bb67b1ae521c48a4b759e5288e337888fd05c49a
Andy <andy@Andys-MacBook-Pro.local>
2024-07-09 17:59:50 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+
+const count = ref(0)
+const text = ref('You did it!')
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld :text="text" :count="count" />
+    </div>
+  </header>
+
+  <main>
+    <button @click="count += 1">Add Count</button>
+    <button @click="text += 1">Change text</button>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
